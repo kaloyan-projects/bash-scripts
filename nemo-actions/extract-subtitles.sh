b909c912eaa2760999c9f4e4782f56dc8c5853f6
Калоян <kaloyansivanov@proton.me>
2024-11-14 20:13:11 +0200

    Update extract-subtitles.sh

--- nemo-actions/extract-subtitles.sh
+++ nemo-actions/extract-subtitles.sh
@@ -11,21 +11,28 @@ for file in "$@"; do
 
     # Check if subtitle tracks are found
     if [ $track_count -eq 0 ]; then
-        zenity --info --text "No subtitle tracks found in $file"
+        zenity --error --text "No subtitle tracks found in $file"
         continue
     fi
 
     # Loop through subtitle streams and extract each one
     for ((i=0; i<$track_count; i++)); do
-        output_file="$(dirname "$(readlink -f "$file")")/${base_name}_subtitle_$i.srt"
+
+        # Get subtitle codec
+        codec=$(ffprobe -v error -select_streams s:"$i" -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "$file")
+        if [[ "$codec" == "ass" ]]; then
+            output_file="$(dirname "$(readlink -f "$file")")/${base_name}_subtitle_$i.ass"
+        else
+            output_file="$(dirname "$(readlink -f "$file")")/${base_name}_subtitle_$i.srt"
+        fi
         
         # Extract the subtitle using ffmpeg
-        ffmpeg -i "$file" -map 0:s:"$i" -c:s srt "$output_file"
+        ffmpeg -i "$file" -map 0:s:"$i" "$output_file"
 
         if [ $? -eq 0 ]; then
             zenity --info --text "Extracted subtitle track $i to $output_file"
         else
-            zenity --info --text "Failed to extract subtitle track $i from $file"
+            zenity --error --text "Failed to extract subtitle track $i from $file"
         fi
     done
 done
